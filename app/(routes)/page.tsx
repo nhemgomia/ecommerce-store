e2f2d40a90cc134831a661d6f5c5a525ade7c01a
Nehemiah Renel Gomia <nhemgomia@gmail.com>
2023-07-25 07:54:04 +0800

    Implement Billboard in store homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,17 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard('ec45388b-836b-4029-a650-89154dd79c7e');
   return ( 
-    <div>Hello from Store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
    );
 }
  
